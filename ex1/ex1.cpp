2045b59cce05c1bd7fd71d1a431bdbef89541304
c0admin <mustafagldren@gmail.com>
2023-01-20 01:12:58 +0300

    Proje dosyası ekle.

---
+++ ex1/ex1.cpp
@@ -0,0 +1,20 @@
+﻿// Faktoriyel Hesaplama
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    int fakt=1;
+    cout << "Faktoriyeli alinacak deger = ";
+    cin >> x;
+
+    for ( ; x > 1; x--) {
+        fakt = fakt * x;
+    }
+    cout << "Sonuc = " << fakt << endl;
+        
+    system("pause");
+    return 0;
+}
+
